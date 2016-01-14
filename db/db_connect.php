97ae8f30ab090fe9a5b63feacc82db0c43f297e0
YoannLth <yoann.lathuiliere@gmail.com>
2016-01-14 11:07:45 +0100

    Database connection with Open Shift

--- db/db_connect.php
+++ db/db_connect.php
@@ -5,9 +5,15 @@
 	$pass = 'secret';
 	$db = 'mymovies';
 	
+	$dbHost = getenv('OPENSHIFT_MYSQL_DB_HOST');
+	$dbName = getenv('OPENSHIFT_GEAR_NAME');
+	$dbUser = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
+	$dbPassword = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
+	
+	
 	// Connexion
 	try {
-		$dbh = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
+		$dbh = new PDO("mysql:host=dbHost;dbname=$dbName;charset=utf8", $dbUser, $dbPassword);
 	} catch (PDOException $e) {
 		print "Erreur !: " . $e->getMessage() . "<br/>";
 		die();
